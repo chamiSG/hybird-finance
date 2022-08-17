cea19d43113fd59c2d4eb01900051a91b6785aa4
Sam <snoblefuentes@gmail.com>
2022-08-17 20:55:01 +0900

    update ui

--- src/views/Invest/index.tsx
+++ src/views/Invest/index.tsx
@@ -2,7 +2,7 @@ import { Container, Box, Divider } from "@material-ui/core";
 import "./invest.scss";
 import Banner from "../../components/Banner";
 import { bannerText } from "../../constants/bannerText"
-import PriceCard from "src/components/PriceCard";
+import { Deposit, Lock, Withdraw } from "src/components/Card";
 import Chart from "src/components/Chart";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 
@@ -18,28 +18,20 @@ function Invest() {
           display={'grid'}
           justifyContent={"center"}
           gridAutoColumns={"1fr"} 
-          gridTemplateColumns={"1fr 1fr 1fr 1fr"} 
-          gridRowGap={isVerySmallScreen ? "16px" : "16px"} 
-          gridColumnGap={"16px"}
+          gridTemplateColumns={"1fr 1fr 1fr"} 
+          gridRowGap={isVerySmallScreen ? "16px" : "4rem"} 
+          gridColumnGap={"4rem"}
+          pb={"10rem"}
         >
           <Box display={'flex'} alignItems={"center"}>
-            <PriceCard label={'Current Price'} value={15.31}></PriceCard>
-            {!isVerySmallScreen && <Divider orientation="vertical" className="price-card-divider" flexItem />}
+            <Deposit></Deposit>
           </Box>
           <Box display={'flex'} alignItems={"center"}>
-            <PriceCard label={'Infinity Pool Value'} value={15.31}></PriceCard>
-            {!isVerySmallScreen && <Divider orientation="vertical" flexItem className="price-card-divider" />}
+            <Lock></Lock>
           </Box>
           <Box display={'flex'} alignItems={"center"}>
-            <PriceCard label={'Token Holders'} value={15.31}></PriceCard>
-            {!isVerySmallScreen && <Divider orientation="vertical" flexItem className="price-card-divider" />}
+            <Withdraw></Withdraw>
           </Box>
-          <Box display={'flex'} alignItems={"center"}>
-            <PriceCard label={'Treasury Value'} value={15.31}></PriceCard>
-          </Box>
-        </Box>
-        <Box display={'flex'} justifyContent={"center"} gridGap={"1rem"} pb={"10rem"}>
-          <Chart></Chart>
         </Box>
       </Container>
     </div>
